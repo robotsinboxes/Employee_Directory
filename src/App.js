de00f4e6aef49d5ab8ef07efad6a65515ce1e035
Lisa Goodell <robotsinboxes@gmail.com>
2021-05-20 22:09:55 -0500

    Built out components

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import SearchContainer from './components/SearchContainer/SearchContainer';
+import Wrapper from './components/Wrapper';
+import Title from './components/Title';
+
+function App() {
+  return <SearchContainer />;
+}
+
+export default App;
---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function SearchForm(props) {
+  return (
+    <form className="container">
+      <div className="form-group mx-5">
+        <h2 className="position-relative" htmlFor="search">Employee Directory</h2>
+        <input
+          className="d-inline"
+          onChange={props.handleInputChange}
+          value={props.search}
+          name="search"
+          type="text"
+          className="form-control"
+          placeholder="Search Employees"
+          id="search"
+        />
+        <button onClick={props.handleFormSubmit} className="btn btn-primary mt-3">
+          Search
+        </button>
+      </div>
+    </form>
+  )
+};
+
+export default SearchForm;
